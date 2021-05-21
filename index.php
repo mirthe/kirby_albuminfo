34c56478779264fbbc28add65d02458e427aa349
Mirthe Valentijn <mirthe@aanzee.nl>
2021-05-21 17:45:26 +0200

    Init plugin

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+
+// TODO opschonen en aanbieden?
+// https://getkirby.com/docs/guide/plugins/best-practices
+
+Kirby::plugin('mirthe/albuminfo', [
+    'options' => [
+        'cache' => true
+    ],
+    'tags' => [
+        'albuminfo' => [
+            'attr' =>[
+                'artist',
+                'title'
+            ],
+            'html' => function($tag) {
+                
+                $albumartist = urlencode($tag->artist);
+                $albumtitle = urlencode($tag->title);
+
+                $apikey = option('lastfm.apiKey');
+                $url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=$apikey&format=json&artist=$albumartist&album=$albumtitle";
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_USERAGENT, 'Test Mirthe.org');
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                $output = curl_exec($ch);
+                curl_close ($ch);
+
+                $albuminfojson = json_decode($output,true);
+                
+                $mijnoutput = '<div class="setlist" style="overflow: auto;">';
+                $mijnoutput .= '<img src="'.$albuminfojson['album']['image']['3']['#text'].'" alt="" class="floatleft" style="margin-right: 1rem;">';
+                $mijnoutput .= '<p>'.$albuminfojson['album']['artist']."<br>";
+                $mijnoutput .= '<a href="'.$albuminfojson['album']['url'].'" title="Bekijken op Last.fm">'.$albuminfojson['album']['name']."</a></p>";
+
+                // TODO use collapse
+                // if( isset( $albuminfojson['album']['wiki'] ) ){
+                //     $mijnoutput .= "<p>".$albuminfojson['album']['wiki']['summary']."</p>";
+                // }
+
+                foreach ($albuminfojson['album']['tracks'] as $tracks) {
+                    if( count($tracks) > 0){
+                        $mijnoutput .= "<ul class=\"songs\">";
+                        for($i = 0; $i < count($tracks); $i++) {
+                            $mijnoutput .= '<li>'. $tracks[$i]['name'] . "</li>";
+                        }
+                        $mijnoutput .= "</ul>";
+                    }
+                    else {
+                        $mijnoutput .= "<p><em>De tracklist is niet bekend bij de Last.fm API.</em></p>";
+                    }
+                }
+
+                $mijnoutput .= '</div>';
+               
+                return $mijnoutput;
+                }
+            ]
+        ]
+    ]);
+?>
