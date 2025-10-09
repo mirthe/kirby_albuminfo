2ab5f1d0b1b617752797ec8cb88f8c8034266c12
Mirthe Valentijn <me@mirthe.org>
2025-10-09 20:48:58 +0200

    Add user agent to curl to precent being blocked by CloudFlare

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ Kirby::plugin('mirthe/albuminfo', [
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_USERAGENT, $site->title());
                 $output = curl_exec($ch);
                 curl_close ($ch);
 
